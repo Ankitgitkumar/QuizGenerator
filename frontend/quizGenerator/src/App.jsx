57a16170e25f3e58b0ecc30086e36bd1ebe507b9
Ankitgitkumar <ankitkumar26125@gmail.com>
2025-07-10 01:36:24 +0530

    added student features

--- frontend/quizGenerator/src/App.jsx
+++ frontend/quizGenerator/src/App.jsx
@@ -1,17 +1,28 @@
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
 import React, { useState } from 'react'
-import CreateQuiz from './Components/teacher/CreateQuiz'
+
 import { Routes, Route } from 'react-router-dom'
 
 import './App.css'
 import './index.css'
 
-import Dashboard from './Components/teacher/Dashboard'
+import Dashboard from './components/Teacher/Dashboard.jsx'
 import MyQuizzes from './components/Teacher/MyQuizzes.jsx'
 import QuizView from './components/Teacher/QuizView.jsx'
 import Leaderboard from './components/Teacher/Leaderboard.jsx'
 import FinalResult from './components/Teacher/FinalResult.jsx'
+import CreateQuiz from './components/Teacher/CreateQuiz.jsx'
+import StudentDashboard from './components/Student/Dashboard.jsx'
+import JoinClassroom from './components/Student/JoinClassroom.jsx'
+import PracticeQuiz from './components/Student/PracticeQuiz.jsx'
+import GenerateQuiz from './components/Student/GenerateQuiz.jsx'
+import AvailableQuizzes from './components/Student/AvailableQuizzes.jsx'
+import PreviousAttempts from './components/Student/PreviousAttempts.jsx'
+import PracticeQuizAttempt from './components/Student/PracticeQuizAttempt.jsx'
+import PracticeQuizReview from './components/Student/PracticeQuizReview.jsx'
+
+
 
 function App() {
   return(
@@ -23,6 +34,15 @@ function App() {
       <Route path="/quiz/:id" element={<QuizView />} />
       <Route path="/leaderboard" element={<Leaderboard />} />
       <Route path="/finalresults" element={<FinalResult />} />
+      <Route path="/student/dashboard" element={<StudentDashboard />} />
+      <Route path="/student/join-classroom" element={<JoinClassroom />} />
+      <Route path="/student/practice-quiz" element={<PracticeQuiz />} />
+      <Route path="/student/practice-quiz/generate-quiz" element={<GenerateQuiz />} />
+      <Route path="/student/practice-quiz/available-quizzes" element={<AvailableQuizzes />} />
+      <Route path="/student/practice-quiz/history" element={<PreviousAttempts />} />
+      <Route path={'/student/practice-quiz/attempt/:id'} element={<PracticeQuizAttempt />} />
+      <Route path="/student/practice-quiz/attempt/:id/review" element={<PracticeQuizReview />} />
+
     </Routes>
 
 

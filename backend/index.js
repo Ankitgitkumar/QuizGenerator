d93424f0484d0d944c0d01ef5d0c7b81686f0d8f
Ankitgitkumar <ankitkumar26125@gmail.com>
2025-08-27 14:24:40 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://quizforgeai-generator-24.vercel.app',
   credentials: true
 }));
 
